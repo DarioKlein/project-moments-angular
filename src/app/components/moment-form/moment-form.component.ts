e33254b79cac9fcf51f3901e3c68a825c8cf5cd3
Dario Klein <dariokleinalves@gmail.com>
2026-04-27 04:55:56 -0300

    add moment page and buttons edit and remove

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,56 +1,57 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Moment } from 'src/app/Moment';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Moment } from 'src/app/Moment'
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
-  @Input() btnText!: string;
-  @Output() onSubmit = new EventEmitter<Moment>();
-  momentForm!: FormGroup;
-  submitted = false;
+  @Input() btnText!: string
+  @Input() momentData: Moment | null = null
+  @Output() onSubmit = new EventEmitter<Moment>()
+  momentForm!: FormGroup
+  submitted = false
 
   constructor() {}
 
   ngOnInit(): void {
     this.momentForm = new FormGroup({
-      id: new FormControl(''),
-      title: new FormControl('', [
+      id: new FormControl(this.momentData ? this.momentData.id : ''),
+      title: new FormControl(this.momentData ? this.momentData.title : '', [
         Validators.required,
         Validators.maxLength(50),
         Validators.minLength(3),
       ]),
-      description: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(255),
-      ]),
-      image: new FormControl(''),
-    });
+      description: new FormControl(
+        this.momentData ? this.momentData.description : '',
+        [Validators.required, Validators.maxLength(255)],
+      ),
+      image: new FormControl(this.momentData ? this.momentData.image : ''),
+    })
   }
 
   get title() {
-    return this.momentForm.get('title')!;
+    return this.momentForm.get('title')!
   }
 
   get description() {
-    return this.momentForm.get('description')!;
+    return this.momentForm.get('description')!
   }
 
   onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    this.momentForm.patchValue({ image: file });
+    const file: File = event.target.files[0]
+    this.momentForm.patchValue({ image: file })
   }
 
   submit() {
-    this.submitted = true;
+    this.submitted = true
 
     if (this.momentForm.invalid) {
-      return;
+      return
     }
 
-    console.log(this.momentForm.value);
-    this.onSubmit.emit(this.momentForm.value);
+    console.log(this.momentForm.value)
+    this.onSubmit.emit(this.momentForm.value)
   }
 }
--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { Moment } from 'src/app/Moment'
+import { MessagesService } from 'src/app/services/messages.service'
 import { MomentService } from 'src/app/services/moment.service'
+import { environment } from 'src/environments/environment'
 
 @Component({
   selector: 'app-moment',
@@ -10,9 +13,14 @@ import { MomentService } from 'src/app/services/moment.service'
 })
 export class MomentComponent implements OnInit {
   moment?: Moment
+  baseAPIUrl = environment.baseAPIUrl
+  faTimes = faTimes
+  faEdit = faEdit
   constructor(
     private momentService: MomentService,
     private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -24,5 +32,11 @@ export class MomentComponent implements OnInit {
     this.momentService
       .getMoment(id)
       .subscribe((item) => (this.moment = item.data))
+  }
+
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe()
+    this.messagesService.add('Momento removido com sucesso!')
+    this.router.navigate(['/'])
   }
 }
